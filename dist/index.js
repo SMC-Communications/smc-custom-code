beb838e8ec4c9697b621d25639a9265aa6a39286
Justin Everett <justin@shelleymcarthur.com>
2023-10-12 13:58:02 -0700

    initial

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+(() => {
+  // index.js
+  console.log("Hello World");
+  var $owl = document.querySelector(".owl-carousel");
+  console.log("test");
+})();
---
+++ index.js
@@ -0,0 +1,6 @@
+//import OwlCarousel from owl-carousel
+
+console.log("Hello World")
+
+let $owl = document.querySelector(".owl-carousel")
+console.log("test")
